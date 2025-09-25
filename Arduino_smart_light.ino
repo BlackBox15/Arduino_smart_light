d76414f56784cbf18c92257730ce393fdf5fd00d
Mikhail Kulkov <mk1221@yandex.ru>
2025-09-25 21:10:24 +0400

    Test commit

--- Arduino_smart_light.ino
+++ Arduino_smart_light.ino
@@ -1,9 +1,19 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <NewPing.h>
 
-}
+#define ULTRASONIC_TRIGGER_PIN 2
+#define ULTRASONIC_ECHO_PIN 3
+#define ULTRASONIC_CHECK_PERIOD 1000
+
+NewPing sonar(ULTRASONIC_TRIGGER_PIN, ULTRASONIC_ECHO_PIN);
+int previousMillis;
+
+void setup() { Serial.begin(9600); }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    unsigned long currentMillis = millis();
 
+    if (currentMillis - previousMillis >= ULTRASONIC_CHECK_PERIOD) {
+        // запустить опрос УЗ датчика
+        previousMillis = currentMillis;
+    }
 }
